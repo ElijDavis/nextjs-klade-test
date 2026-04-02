eb1f96fbc97e656d39cedff2711e80c7278e06bd
ElijDavis <elijahadavis@hotmail.com>
2026-04-02 17:34:01 -0400

    Created Directories: men and women Created Files: men/page.tsx and women/page.tsx

--- nexjs-klade-test/app/components/navbar.tsx
+++ nexjs-klade-test/app/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="absolute top-0 left-0 w-full z-50 flex flex-row justify-evenly items-center mt-10 *:p-2">
+    <div className="absolute top-0 left-0 w-full z-50 flex flex-row justify-evenly items-center bg-white/20 p-5 *:p-2">
       <Link className="border-2 rounded-2xl border-orange-600 hover:scale-110" href="/men">men</Link>
       <Link className="border-2 rounded-2xl border-orange-600 hover:scale-110" href="/">Home</Link>
       <Link className="border-2 rounded-2xl border-orange-600 hover:scale-110" href="/women">Women</Link>
--- nexjs-klade-test/app/women/page.tsx
+++ nexjs-klade-test/app/women/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-export default function Home() {
+const WomensPage = () => {
   const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
@@ -12,11 +10,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center">
-      <Image src="/palm.jpg" alt="Palm Tree" width={windowWidth} height={windowWidth * .25} className="bg-black/10"/>
-      <div className="absolute top-50 z-20 flex flex-col items-center mt-10">
-        <h1 className="text-8xl">Port Lovell</h1>
+      <Image src="/palm.jpg" alt="Palm Tree" width={windowWidth} height={400}/>
+      <div className="flex flex-col items-center mt-10">
+        <h1 className="text-8xl">Women</h1>
         <p className="">Carribean style : American Made</p>
       </div>
     </div>
-  );
+  )
 }
+
+export default WomensPage;
