4d60c74da228282049531055d82f1576d4ecce89
ElijDavis <elijahadavis@hotmail.com>
2026-05-02 09:20:02 -0400

    another re-test after removing unecessary webhook

--- nexjs-klade-test/app/page.tsx
+++ nexjs-klade-test/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center">
-      <h1 className="text-4xl font-bold text-white mt-20">Welcome to Port Lovelle</h1>
+      <h1 className="text-4xl font-bold shadow-2xl shadow-blue-950 text-white mt-20">Welcome to Port Lovelle</h1>
     </div>
   );
 }
