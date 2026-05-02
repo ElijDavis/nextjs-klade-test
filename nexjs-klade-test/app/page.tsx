85c388be9033ba6678207d0188def4b95e978b30
ElijDavis <elijahadavis@hotmail.com>
2026-05-02 11:05:15 -0400

    description added

--- nexjs-klade-test/app/page.tsx
+++ nexjs-klade-test/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center">
       <h1 className="text-4xl font-bold text-white mt-20">Port Lovell</h1>
+      <div>
+        <p className="text-lg text-white mt-4">A beautiful coastal town with stunning views and a vibrant community.</p>
+      </div>
     </div>
   );
 }
