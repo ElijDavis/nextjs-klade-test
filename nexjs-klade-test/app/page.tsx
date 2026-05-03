4050594816c99028367b8f4ff10db2b985d4a164
ElijDavis <elijahadavis@hotmail.com>
2026-05-02 21:40:53 -0400

    idk

--- nexjs-klade-test/app/page.tsx
+++ nexjs-klade-test/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center">
       <h1 className="text-4xl font-bold text-white mt-20">Port Lovell</h1>
       <div>
-        <p className="text-lg text-white mt-4">A beautiful coastal town with stunning views and a vibrant community.</p>
+        <p className="text-lg text-white mt-4">Bes clothing in the world</p>
       </div>
     </div>
   );
