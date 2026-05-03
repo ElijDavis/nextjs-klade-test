f04c20828bf29bbd269e21dd70fe6d9c19173ad5
ElijDavis <elijahadavis@hotmail.com>
2026-05-02 21:38:35 -0400

    Test, should work

--- nexjs-klade-test/app/page.tsx
+++ nexjs-klade-test/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-white mt-20">Port Lovell</h1>
       <div>
         <p className="text-lg text-white mt-4">A beautiful coastal town with stunning views and a vibrant community.</p>
-        <p className="text-lg text-white mt-2">Enjoy the serene beaches, explore the charming shops, and indulge in delicious seafood at local restaurants.</p>
       </div>
     </div>
   );
